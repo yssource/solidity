3e68bd7b24353e36f2529c47883c1eff50103751
Daniel Kirchner <daniel@ekpyron.org>
2019-03-15 15:09:28 +0100

    Tests.

---
+++ test/libsolidity/semanticTests/abiEncoderV2/calldata_struct_simple.sol
@@ -0,0 +1,17 @@
+// EVMVersion: >homestead
+pragma experimental ABIEncoderV2;
+
+contract C {
+    struct S { uint256 a; }
+
+    function f(S calldata s) external returns (bytes memory) {
+        return abi.encode(s);
+    }
+
+    function g(S calldata s) external returns (bytes memory) {
+        return this.f(s);
+    }
+}
+// ----
+// f((uint256)): 3 -> 32, 32, 3
+// g((uint256)): 3 -> 32, 32, 3
